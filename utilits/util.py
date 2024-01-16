9a5fb7a12f5549daa1eab87a3ebb4f6e20f2bee4
kawr <kawr@gmail.com>
2024-01-17 02:46:17 +0500

    minimum

--- utilits/util.py
+++ utilits/util.py
@@ -2,8 +2,20 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 
-url = "http://94.73.216.81:6003/sigir_work/hs/API/Check/CheckCont?ContNum=HNKU6137310"
-response = requests.get(url, auth=HTTPBasicAuth('web_api', 'Fcn9KhC85hXnut12'))
+# Замените ContNum на нужный номер контейнера
+cont_num = "HNKU6137310"
+url = f"http://94.73.216.81:6003/sigir_work/hs/API/Check/CheckCont?ContNum={cont_num}"
 
-print(response.status_code)
-print(response.text)
+# Аутентификационные данные
+username = "web_api"
+password = "Fcn9KhC85hXnut12"
+
+# Отправка GET-запроса с базовой аутентификацией
+response = requests.get(url, auth=HTTPBasicAuth(username, password), timeout=10)
+
+# Проверка ответа от сервера
+if response.status_code == 200:
+    print("Запрос выполнен успешно. Ответ сервера:")
+    print(response.text)
+else:
+    print(f"Ошибка выполнения запроса. Код состояния: {response.status_code}")
